b7b7c11feb013c083b8f98045ba4f0fb8d586fe7
Jeremy Lindblom <jeremeamia@gmail.com>
2011-08-01 19:33:37 -0700

    Completed installation script.

--- install.php
+++ install.php
@@ -7,19 +7,30 @@
 		<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 		<link href="assets/boilerplate.css" rel="stylesheet">
 		<style>
-			body {margin: 1em 2em; color: #666;}
+			body {color: #666;}
+			div#content {width: 800px; margin: 1em auto;}
 			h1 {font-size: 2em; margin-bottom: 1em; border-bottom: 3px solid #666;}
 			h2 {font-size: 1.5em; margin-bottom: 1em;}
+			hr {border: 0; color: #666; background-color: #666; height: 3px; margin: 2em 0;}
 			p.check {padding: 1em; margin-bottom: 1em; border: 3px solid #666; font-weight: bold;}
 			p.yes {background-color: #cfc; color: #060; border-color: #8c8;}
-			p.yes:before {content: '[SUCCESS]'; padding-right: 0.5em; opacity: 0.5;}
+			p.yes:before {content: '[PASS]'; padding-right: 0.5em; opacity: 0.5;}
 			p.no {background-color: #fcc; color: #600; border-color: #c88;}
 			p.no:before {content: '[FAIL]'; padding-right: 0.5em; opacity: 0.5;}
+			code {background-color: #eee; font-weight: bold;}
 		</style>
 	</head>
 	<body>
-		<h1>Install Application</h1>
+		<div id="content">
+			<h1>Install Application</h1>
 <?php
+
+echo '<h2>Sanity Checks:</h2>';
+
+$check = (file_exists('config.php') AND file_exists('schema.sql') AND file_exists('classes')) ? 'yes' : 'no';
+echo '<p class="check '.$check.'">Installation requires all application files to exist.</p>';
+
+//------------------------------------------------------------------------------
 
 echo '<h2>Compatibility Checks:</h2>';
 
@@ -42,7 +53,7 @@ echo '<p class="check '.$check.'">Application requires the MySQLi PHP extension.
 
 //------------------------------------------------------------------------------
 
-echo '<h2>Test Connections:</h2>';
+echo '<h2>Configuration:</h2>';
 
 $config = include 'config.php';
 
@@ -50,20 +61,67 @@ $config = include 'config.php';
 $connection = new MySQLi(
 	isset($config['database']['host']) ? $config['database']['host'] : NULL,
 	isset($config['database']['username']) ? $config['database']['username'] : NULL,
-	isset($config['database']['password']) ? $config['database']['password'] : NULL,
-	isset($config['database']['name']) ? $config['database']['name'] : NULL
+	isset($config['database']['password']) ? $config['database']['password'] : NULL
 );
 $check = ( ! $connection->connect_error) ? 'yes' : 'no';
 echo '<p class="check '.$check.'">Application requires valid database connection details.</p>';
 
-// @TODO Test Netflix connection
+// Test Netflix API key
+$consumer_key = isset($config['netflix']['api_key']) ? $config['netflix']['api_key'] : NULL;
+if ($consumer_key)
+{
+	$url = 'http://api.netflix.com/catalog/titles/autocomplete?oauth_consumer_key='.$consumer_key.'&term=The%20Net&output=json';
+	$api_call = @json_decode(file_get_contents($url));
+	$check = ($api_call !== NULL) ? 'yes' : 'no';
+}
+else
+{
+	$check = 'no';
+}
+echo '<p class="check '.$check.'">Application requires valid api key for Netflix Develop API.</p>';
 
 //----------------------------------------------------------------------------
 
 echo '<h2>Setup Database:</h2>';
 
-// @TODO Setup database
+// Setup Database
+$database_name = isset($config['database']['name']) ? $config['database']['name'] : NULL;
+$success = FALSE;
+if ($database_name)
+{
+	$database_exists = $connection->select_db($database_name);
+	if ( ! $database_exists)
+	{
+		$sql = 'CREATE DATABASE IF NOT EXISTS '.$connection->real_escape_string($database_name);
+		$database_exists = $connection->query($sql);
+		header('Location: '.$_SERVER['REQUEST_URI']);
+		exit;
+	}
+
+	if ($database_exists)
+	{
+		$sql = file_get_contents('schema.sql');
+		$previous_query_successful = TRUE;
+		foreach (explode(';', $sql) as $query)
+		{
+			$query = trim($query);
+			if ($previous_query_successful AND $query)
+			{
+				$previous_query_successful = $connection->query($query);
+			}
+		}
+		$success = $previous_query_successful;
+	}
+}
+$check = $success ? 'yes' : 'no';
+echo '<p class="check '.$check.'">Application requires database with correct schema in place.</p>';
 
 ?>
+		<hr>
+	
+		<p><strong>NOTE:</strong> If all items are marked in green as "PASS", then the
+		application is ready to use and you may delete &ldquo;<code>install.php</code>&rdquo;.</p>
+
+		</div>
 	</body>
 </html>
