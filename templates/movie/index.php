cd65df0cb0382c60abfd69d39422feef294663ff
Jeremy Lindblom <jeremeamia@gmail.com>
2011-08-10 00:15:56 -0700

    tweaks to index page design

--- templates/movie/index.php
+++ templates/movie/index.php
@@ -3,7 +3,10 @@
 <strong>Movies in Library:</strong>
 <ul>
 	<? foreach ($movies as $movie): ?>
-		<li><?= $movie->get('title') ?> (<?= $movie->get('year') ?>) <?= $view->getHelper('html')->link('remove', array('movie', 'remove', $movie->get('id'))) ?></li>
+		<li>
+			<?= $movie->get('title') ?> (<?= $movie->get('year') ?>)
+			<?= $view->getHelper('html')->link('remove', array('movie', 'remove', $movie->get('id')), array('class' => 'remove-link')) ?>
+		</li>
 	<? endforeach; ?>
 	<li><strong><?= $view->getHelper('html')->link('+ Add a Movie', array('movie', 'lookup')) ?></strong></li>
 </ul>
